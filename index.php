79e86f6a749b524e0ea36edbeee83266b8e4ccf2
pps-MP <pps_p@mail.ru>
2021-12-05 19:58:30 +0500

    Start Commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+echo "hello world!";
+echo "<br>";
+echo "hello";
+echo '<br>';
+echo 55;
+echo '<br>';
+echo '<button>click!</button>';
+echo '<br>';
+echo date ('Y-m-d H:i:s');
+echo '<br>';
+echo 2 + 1;
+echo '<br>';
+echo 5 - 1;
+echo '<br>';
+echo 6 * 4;
+echo '<br>';
+echo 36 / 4;
+echo '<br>';
+echo 5+2*2;
+echo '<br>';
+echo (5+2)*2;
+echo '<br>';
+echo 2**3;
+echo '<br>';
+echo 10%3;
+echo '<br>';
+// matematics operation
