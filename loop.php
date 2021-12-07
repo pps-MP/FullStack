cc6ba1196084c442339624840a63fd496c9d9e74
pps-MP <pps_p@mail.ru>
2021-12-07 21:02:36 +0500

    поправил задачу

--- loop.php
+++ loop.php
@@ -24,25 +24,26 @@ echo '</select>';
 
 echo '<select>';
 echo '<option selected disabled>-- Выберите значение --</option>';
-for ($i = 1; $i < 10; $i++){
-    echo "<option> $i </option>";
+for ($f = 0; $f < 10; $f++){
+    echo "<option> $f </option>"
+    ;
 }
 echo '</select>';
 
 echo '<hr>';
 $cel = 0;
-$far = -51;
+$far = -50;
 while ($far < 51){
-    $cel = ($far - 32)*5/9;
-    echo $far . ' F = ' . round($cel, 2) . ' C' . '<br>';
+    $cel = ($far - 32) * 5 / 9;
+    echo $far . ' F = ' . round($cel, 2) . ' C' . '<br>';// round округление до 2х знаков после запятой
     $far++;
 }
 
 echo '<hr>';
-$cel = -51;
+$cel = -50;
 $far = 0;
 while ($cel < 51){
-    $far = $cel*9/5 + 32;
+    $far = $cel * 9 / 5 + 32;
     echo $cel . ' C = ' . round($far, 2) . ' F' . '<br>';
     $cel++;
 }
