f58ca9c729e83a54980725482b1b95bbd9fad59d
pps-MP <pps_p@mail.ru>
2021-12-07 21:59:36 +0500

    222

--- array.php
+++ array.php
@@ -31,3 +31,31 @@ echo '<hr>';
 foreach ($numbers as $key => $value ){
 echo "$key : $value <br>";
 }
+
+$n_numbers = [];
+for ($i = 0; $i < 10; $i++){ // заполняем массив значениями от 0 до 9
+    $n_numbers[] = $i;
+}
+
+$n_numbers[10] = 100; // добавляем(заменяем) данные в массиве по индексу 10, будет ключ 10 со значением 100
+$n_numbers[44] = 4 * 8;// добавляем(заменяем) данные в массиве , будет ключ 44 со значением 32
+
+echo '<pre>';
+print_r($n_numbers);
+
+$person = [ // ассоциативный массив
+    'name' => 'Voo',
+    'age' => 30,
+    'city' => 'London',
+    'language' => 'php'
+];
+$person['car'] = true; // добавляем ключ car и значение true (1) в массив
+$person[ = ''; // добавляем ключ car и значение true (1) в массив
+
+
+echo '<pre>';
+print_r($person);
+
+unset($person['name']); // полностью удаляем ключ из массива
+echo '<pre>';
+print_r($person);
